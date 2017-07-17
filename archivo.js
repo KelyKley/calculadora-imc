bc73626b2a9656d9b593edacd01b51c92a7ccaba
MelanieOC <melanie.ocharan@gmail.com>
2017-07-17 11:58:09 -0500

    calculador imc

--- archivo.js
+++ archivo.js
@@ -1,1 +1,7 @@
-console.log('calculadora IMC');
+var altura = Number(prompt('Tu altura actual es (en cm):'))/100;
+var peso = Number(prompt('Tu peso es (en kg):'));
+function calculadoraIMC() {
+  var IMC = peso/(altura*altura);
+  alert('Tu IMC es: ' + IMC.toFixed(2) + ' Kg/m2');
+}
+calculadoraIMC();
