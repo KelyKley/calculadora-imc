0e338a5de8cc0eb46fcfc8a3f2b9d4a5e3747361
kelyanamuro <kelyanamurohuamani@gmail.com>
2017-07-17 10:06:29 -0700

    Ejercicio subido a otra rama por Kely

--- archivo.js
+++ archivo.js
@@ -1,1 +1,9 @@
-console.log('calculadora IMC');
+
+	function calcular(){
+	var peso = parseInt(prompt("Ingresa tu peso(Kg):", 60));
+	var estatura = prompt("Ingresa tu estatura(Mts.):", 1.65);
+
+	var operando = Math.round(peso / (estatura*estatura));
+	alert("Tu IMC (indice de masa corporal) es: " + operando );
+}
+calcular();
