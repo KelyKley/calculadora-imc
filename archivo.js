500bb44e44a2a679822e6be147488715461b6a4b
MelanieOC <melanie.ocharan@gmail.com>
2017-07-17 14:33:33 -0500

    html

--- archivo.js
+++ archivo.js
@@ -1,8 +1,19 @@
 
 function calculadoraIMC() {
-	var altura = Number(prompt("Ingresa tu estatura(Mts.):", 1.65));
-	var peso = Number(prompt("Ingresa tu peso(Kg):", 60));
-  	var IMC = peso/(altura*altura);
-  	alert('Tu IMC (indice de masa corporal) es: ' + IMC.toFixed(2) + ' Kg/m2');
+	var altura = document.getElementById('altura').value;
+	var peso = document.getElementById('peso').value;
+  var IMC = peso/(altura*altura);
+  var resultado = 'Tu IMC (indice de masa corporal) es: ' + IMC.toFixed(2) + ' Kg/m2';
+  var estado;
+  if(IMC < 18.5){
+    estado = 'Bajo Peso';
+  } else if(IMC < 25 && IMC > 18.5){
+    estado = 'Peso Normal';
+  } else if(IMC >= 25 && IMC < 30){
+    estado = 'Sobrepeso';
+  } else {
+    estado = 'Obesidad';
+  }
+  document.getElementById('resultado').innerHTML=resultado + '<br>' + 'Tu estado es: ' + estado;
 }
-calculadoraIMC();
+//calculadoraIMC();
